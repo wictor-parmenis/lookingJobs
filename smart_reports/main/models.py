4c101ce54632cf3d1945ef025c475665528e0684
wictor-parmenis <wictortec@gmail.com>
2021-03-23 22:01:20 -0300

    Configure initial models

--- smart_reports/main/models.py
+++ smart_reports/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Reports(models.Model):
+    title = models.CharField(max_length=240)
+
+    def __str__(self):
+        return self.title
+
+
+class Item(models.Model):
+    report = models.ForeignKey(Reports, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
--- smart_reports/main/views.py
+++ smart_reports/main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(response):
     return HttpResponse('<h1>Report Job</h1>')
+
+
+def newsletter(response):
+    return HttpResponse('<h1>Newsletter here</h1>')
